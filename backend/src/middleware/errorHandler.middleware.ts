dc55fb28ecb6070b6515604225fba7523097385d
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-09-08 19:35:51 +0530

    changed

--- backend/src/middleware/errorHandler.middleware.ts
+++ backend/src/middleware/errorHandler.middleware.ts
@@ -1,3 +1,29 @@
 import { ErrorRequestHandler,Response } from "express";
 import {ZodError} from "zod";
 import { HTTP_STATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
+import { logger } from "../utils/logger";
+import { ErrorCodeEnum } from "../enum/errorCode.enum";
+
+const formatZodeError=(res:Response,error:ZodError)=>{
+    const errors=error?.issues?.map((err)=>({
+        field:err.path.join('.'),
+        message:err.message
+    }));
+
+    return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        message:"Validation Error",
+        errorCode:ErrorCodeEnum.VALIDATION_ERROR,
+        errors
+    })
+}
+
+
+export const errorHandler:ErrorRequestHandler=(
+    error,
+    req,
+    res,
+    next
+):any=>{
+    logger.error(`Error on path`)
+}
