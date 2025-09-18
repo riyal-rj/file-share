8754b30ef4c00110db55d786f20c285e30d3d343
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-09-18 12:19:31 +0530

    deleted frontend

--- frontend/src/app/hook.ts
+++
@@ -1,6 +0,0 @@
-import { type TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
-import { type RootState, store } from "./store"
-
-export type AppDispatch = typeof store.dispatch // Type for dispatch function
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
--- frontend/src/constant/index.ts
+++
@@ -1,8 +0,0 @@
-export const LAYOUT_VIEW = {
-  GRID: "GRID",
-  LIST: "LIST",
-} as const;
-
-export type LayoutViewType = keyof typeof LAYOUT_VIEW;
-
-export const IMAGE_EXTENSIONS = ["JPG", "JPEG", "PNG", "GIF", "SVG"];
